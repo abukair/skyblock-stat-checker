3f3db61709f5329c00f113c955b00d267ad72fe6
abukair <ahmbukair@gmail.com>
2024-10-05 22:17:35 -0700

    Added index.js,metadata.json

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// Register the command
+register("command", (arg, arg2) => {
+    argument = arg;
+    // Check if both arguments are provided
+    ifssss (argument) {
+        let link = `https://sky.shiiyu.moe/stats/${argument}`;
+        // Send the link to the player client-side
+        ChatLib.chat(link);
+    } else {
+        // Send an error message if both arguments are not provided
+        ChatLib.chat("§cUsage: /view [ign]");
+    }
+}).setName("view");
